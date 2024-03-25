39db714995e896aa039332824fbd82479e15cb5b
dattaram sugave <dattaram.sugave@gmail.com>
2024-03-25 22:22:19 +0530

    added new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import logto from "./assests/J&S_BLACK.png";
+import bg from "./assests/bg-pic.jpg";
 import SuccessModal from "./components/SuccessModal";
 import { useRef, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
@@ -25,7 +26,7 @@ export default function App() {
 
   const openStoreLocation = () => {
     // Replace the coordinates with your store's latitude and longitude
-    const latitude =10.773500062158613;
+    const latitude = 10.773500062158613;
     const longitude = 76.65748505290128;
 
     // Create a Google Maps URL with the specified coordinates
@@ -35,9 +36,13 @@ export default function App() {
     window.open(mapUrl, '_blank');
   };
 
+  const openWhatsApp=()=>{
+    window.open(`https://wa.me/+919744477141`, '_blank');
+  }
+
   const handleCallClick = () => {
     window.location.href = `tel:+919744477141`;
-  
+
   };
 
   const addUserDetails = async (event) => {
@@ -58,8 +63,8 @@ export default function App() {
         },
       ])
       .single();
-  console.log(data)
- 
+    console.log(data)
+
     setShowModal(true);
   };
   return (
@@ -68,10 +73,11 @@ export default function App() {
       <img
         src={logto}
         alt="Jamal Logo"
-        className="w-[150px]  scale-150 mx-auto mb-4"
+        className="scale-300 max-w-[100px] mx-auto mb-4"
+        style={{ transform: 'scale(2)' }}
       />
       <section class="mb-32">
-        <div class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://mdbcdn.b-cdn.net/img/new/textures/full/284.jpg')]"></div>
+        <div class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat " style={{ backgroundImage: `url(${bg})`, backgroundPosition: '50%', backgroundRepeat: 'no-repeat' }}></div>
         <div class="container px-6 md:px-12">
           <div class="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
             <div class="flex flex-wrap">
@@ -129,6 +135,33 @@ export default function App() {
               </div>
               <div class="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
                 <div class="flex flex-wrap">
+
+                  <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
+                    <div class="flex items-start">
+                      <div class="shrink-0">
+                        <div onClick={openWhatsApp} class="inline-block cursor-pointer rounded-md bg-primary-100 p-4 text-primary">
+                          <svg width="24px" height="24px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M16 31C23.732 31 30 24.732 30 17C30 9.26801 23.732 3 16 3C8.26801 3 2 9.26801 2 17C2 19.5109 2.661 21.8674 3.81847 23.905L2 31L9.31486 29.3038C11.3014 30.3854 13.5789 31 16 31ZM16 28.8462C22.5425 28.8462 27.8462 23.5425 27.8462 17C27.8462 10.4576 22.5425 5.15385 16 5.15385C9.45755 5.15385 4.15385 10.4576 4.15385 17C4.15385 19.5261 4.9445 21.8675 6.29184 23.7902L5.23077 27.7692L9.27993 26.7569C11.1894 28.0746 13.5046 28.8462 16 28.8462Z" fill="#BFC8D0" />
+                            <path d="M28 16C28 22.6274 22.6274 28 16 28C13.4722 28 11.1269 27.2184 9.19266 25.8837L5.09091 26.9091L6.16576 22.8784C4.80092 20.9307 4 18.5589 4 16C4 9.37258 9.37258 4 16 4C22.6274 4 28 9.37258 28 16Z" fill="url(#paint0_linear_87_7264)" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16C2 18.5109 2.661 20.8674 3.81847 22.905L2 30L9.31486 28.3038C11.3014 29.3854 13.5789 30 16 30ZM16 27.8462C22.5425 27.8462 27.8462 22.5425 27.8462 16C27.8462 9.45755 22.5425 4.15385 16 4.15385C9.45755 4.15385 4.15385 9.45755 4.15385 16C4.15385 18.5261 4.9445 20.8675 6.29184 22.7902L5.23077 26.7692L9.27993 25.7569C11.1894 27.0746 13.5046 27.8462 16 27.8462Z" fill="white" />
+                            <path d="M12.5 9.49989C12.1672 8.83131 11.6565 8.8905 11.1407 8.8905C10.2188 8.8905 8.78125 9.99478 8.78125 12.05C8.78125 13.7343 9.52345 15.578 12.0244 18.3361C14.438 20.9979 17.6094 22.3748 20.2422 22.3279C22.875 22.2811 23.4167 20.0154 23.4167 19.2503C23.4167 18.9112 23.2062 18.742 23.0613 18.696C22.1641 18.2654 20.5093 17.4631 20.1328 17.3124C19.7563 17.1617 19.5597 17.3656 19.4375 17.4765C19.0961 17.8018 18.4193 18.7608 18.1875 18.9765C17.9558 19.1922 17.6103 19.083 17.4665 19.0015C16.9374 18.7892 15.5029 18.1511 14.3595 17.0426C12.9453 15.6718 12.8623 15.2001 12.5959 14.7803C12.3828 14.4444 12.5392 14.2384 12.6172 14.1483C12.9219 13.7968 13.3426 13.254 13.5313 12.9843C13.7199 12.7145 13.5702 12.305 13.4803 12.05C13.0938 10.953 12.7663 10.0347 12.5 9.49989Z" fill="white" />
+                            <defs>
+                              <linearGradient id="paint0_linear_87_7264" x1="26.5" y1="7" x2="4" y2="28" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#5BD066" />
+                                <stop offset="1" stop-color="#27B43E" />
+                              </linearGradient>
+                            </defs>
+                          </svg>
+                        </div>
+                      </div>
+                      <div onClick={openWhatsApp} class="ml-6 grow">
+                        <p class="mb-2 font-bold dark:text-white">WhatsApp</p>
+                        <div className="text-md tracking-wide font-semibold w-40 dark:text-neutral-200">
+                          +91 97444 77141
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                   <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                     <div class="flex items-start">
                       <div class="shrink-0">
@@ -191,7 +224,7 @@ export default function App() {
                         <p class="mb-2 font-bold dark:text-white">
                           Mobile Number
                         </p>
-                        <div  className="text-md tracking-wide font-semibold w-40 dark:text-neutral-200">
+                        <div className="text-md tracking-wide font-semibold w-40 dark:text-neutral-200">
                           +91 97444 77141
                         </div>
                       </div>
